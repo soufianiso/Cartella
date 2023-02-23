465b168e05c95c4e11d3c1db5b0e34762b628257
soufiane <soufiantamasi@gmail.com>
2023-02-23 15:00:36 +0100

    enhance the views in cart

--- project/cart/views.py
+++ project/cart/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, RedirectView
 from .models import Order
 # Create your views here.
 
-class AddToCart(ListView):
-    context_object_name= 'orderitem'
+class CartView(ListView):
+    context_object_name= 'items'
 
     def get_queryset(self):
         order, created = Order.objects.get_or_create(session_id = self.request.session.session_key, compeleted=False )
         order.save()
-        item = order.orderitem_set.all()
-        return item
+        items = order.orderitem_set.all()
+        return items
          
+class AddToCart(RedirectView):
+    url = '/'
